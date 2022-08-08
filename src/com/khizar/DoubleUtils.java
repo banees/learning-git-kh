accc2d3013b0cea91a20bb46e27cda25096f762b
Bilal Anees <bilalanees18@gmail.com>
2022-08-08 21:43:09 +0500

    Add Double Utils class

---
+++ src/com/khizar/DoubleUtils.java
@@ -0,0 +1,12 @@
+package com.khizar;
+
+public class DoubleUtils {
+    
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+}
